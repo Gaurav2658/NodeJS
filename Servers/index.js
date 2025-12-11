86c079d759fc26890493a02feee497da5f916ab4
Gaurav Bhadoriya <bhadoriyagaurav1@gmail.com>
2025-12-11 14:14:30 +0530

    "Handling URL"

--- Servers/index.js
+++ Servers/index.js
@@ -1,16 +1,22 @@
-const http = require("http")
-const fs = require("fs")
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
     const log = `${Date.now()} ${req.url}: New data added\n`;
+    if(req.url === "/favicon.ico") return res.end();
+    const myUrl = url.parse(req.url, true)
+    console.log(myUrl);
     fs.appendFile("log.txt", log, (err, data) => {
         if(err){
             console.log("An error is thrown")
         }else{
-            switch(req.url){
+            switch(myUrl.pathname){
                 case '/': res.end("Home page");
                 break
-                case '/about': res.end("about Page");
+                case '/about': 
+                const userName = myUrl.query.myname
+                res.end(`Hi ${userName}`); 
                 break
                 default: res.end("404 Not found")
                 break
