bb7ca058b4feb37533bb6cefca37a7259e6420ca
Gaurav Bhadoriya <bhadoriyagaurav1@gmail.com>
2025-12-15 14:27:44 +0530

    Rest API crud Operation

--- Project-01/index.js
+++ Project-01/index.js
@@ -1,11 +1,12 @@
 const express = require("express")
-const users = require("./MOCK_DATA.json")
+let users = require("./MOCK_DATA.json")
 const app = express();
-const url = require("url")
+const fs = require("fs");
+const { error } = require("console");
 const PORT = 8000;
 
 app.listen(PORT, () => console.log("Starting the server"))
-// console userId = url.
+app.use(express.urlencoded({extended: false}))
 
 app.get("/users", (req, res) => {
     const html = 
@@ -19,24 +20,51 @@ app.get("/api/users", (req, res) => {
     return res.json(users)
 })
 
+//Routes
 app.route("/api/users/:id").get((req, res) => {
     const id = Number(req.params.id);
     const user = users.find((user) => user.id == id);
     return res.json(user)
 }).patch((req, res) => {
-    return res.json({status: "pending"})
+  const id = Number(req.params.id);
+  const {first_name, last_name, email, gender, job_title} = req.body;
+  let user = users.find(user => user.id === id);
+  if (!user) {
+    return res.status(404).send("User not found");
+  }
+  user.first_name = first_name || user.first_name;
+  user.last_name  = last_name  || user.last_name;
+  user.email      = email      || user.email;
+  user.gender     = gender     || user.gender;
+  user.job_title  = job_title  || user.job_title;
+    fs.writeFile("MOCK_DATA.json",JSON.stringify(users, null, 2),(error) => {
+      if (error) {
+        return res.status(500).send("Failed to save data");
+      }
+      res.status(200).json({message: `User with ID ${id} updated successfully`,user});
+    }
+  );
 }).delete((req, res) => {
-    return res.json({status: "pending"})
-})
+  const  id  = Number(req.params.id);
+  console.log("ID of the user", id)
+  console.log("ID of the user", users)
+  fs.writeFile("MOCK_DATA.json", JSON.stringify(users.filter(user => user.id !== id)), (error, data) => {
+    return res.status(200).send(`User with ID ${id} has been deleted.`);
+  });
+});
 
 // app.get("/api/users/:id", (req, res) => {
-//     const id = Number(req.params.id);
+//     const id = Number(req.params.id); 
 //     const user = users.find((user) => user.id == id);
 //     return res.json(user)
 // })
 
 
 app.post("/api/users", (req, res) => {
-    // TODO: crate new user 
-    return res.json({status: "pending"})
+    const body = req.body;
+    users.push({...body, id: users.length + 1});
+    fs.writeFile("MOCK_DATA.json", JSON.stringify(users), (error, data) => {
+        return res.json({status: "success", id: users.length})
+    })
+    // return res.json({status: "pending"})
 })
